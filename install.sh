65ed9627c093dcf53e939cd19d0029fce953af3b
LittleKey <LittleKeyRain@gmail.com>
2017-06-15 16:25:06 +0800

    update install script for flake8 config

--- install.sh
+++ install.sh
@@ -27,5 +27,13 @@ else
     echo '[WARNING] git config file is not exists.'
 fi
 
+# link flake8 config file
+if [ -e ${CURRENT_DIR}/flake8 ];
+then
+    ln -f ${CURRENT_DIR}/flake8 ${HOME}/.flake8
+else
+    echo '[WARNING] flake8 config file is not exists.'
+fi
+
 # install k-vim
 (cd ${CURRENT_DIR}/k-vim || exit) && sh -x install.sh
