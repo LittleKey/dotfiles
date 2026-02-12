12d476b0acf43499f6adfc3c560263a55a2957b8
LittleKey <LittleKeyRain@gmail.com>
2026-02-12 10:17:14 +0000

    refactor(install.sh): replace backticks with $() and fix starship path

--- install.sh
+++ install.sh
@@ -1,46 +1,39 @@
 #!/bin/bash
 
-CURRENT_DIR=`pwd`
+CURRENT_DIR=$(pwd)
 
 # append zshrc config
-if [ -e ${CURRENT_DIR}/zshrc ];
-then
-    cat zshrc >> ${HOME}/.zshrc
+if [ -e ${CURRENT_DIR}/zshrc ]; then
+	cat zshrc >>${HOME}/.zshrc
 fi
 
 # link ghostty config
-if [ -e ${CURRENT_DIR}/ghostty ];
-then
-    mkdir -p ${HOME}/.config/ghostty
-    ln -f ${CURRENT_DIR}/ghostty.conf ${HOME}/.config/ghostty/config
+if [ -e ${CURRENT_DIR}/ghostty ]; then
+	mkdir -p ${HOME}/.config/ghostty
+	ln -f ${CURRENT_DIR}/ghostty.conf ${HOME}/.config/ghostty/config
 fi
 
 # link starship
-if [ -e ${CURRENT_DIR}/starship.toml ];
-then
-    ln -f ${CURRENT_DIR}/starship.toml ${HOME}/.config/starship.toml
+if [ -e ${CURRENT_DIR}/starship.toml ]; then
+	mkdir -p ${HOME}/.config/starship
+	ln -f ${CURRENT_DIR}/starship.toml ${HOME}/.config/starship/starship.toml
 fi
 
-# link slate config file
-ln -f ${CURRENT_DIR}/slate ${HOME}/.slate
-
 # link tmux config file
-if [ -e ${CURRENT_DIR}/k-tmux/install.sh ];
-then
-    sh -x ${CURRENT_DIR}/k-tmux/install.sh
+if [ -e ${CURRENT_DIR}/k-tmux/install.sh ]; then
+	sh -x ${CURRENT_DIR}/k-tmux/install.sh
 else
-    echo '[WARNING] t-mux install script is not exists.'
+	echo '[WARNING] t-mux install script is not exists.'
 fi
 
 # link git config file
-if [ -e ${CURRENT_DIR}/gitconfig ];
-then
-    if [ "$(uname -s)" == "Darwin" ]; then
-      brew install git-delta
-    fi
-    ln -f ${CURRENT_DIR}/gitconfig ${HOME}/.gitconfig
+if [ -e ${CURRENT_DIR}/gitconfig ]; then
+	if [ "$(uname -s)" == "Darwin" ]; then
+		brew install git-delta
+	fi
+	ln -f ${CURRENT_DIR}/gitconfig ${HOME}/.gitconfig
 else
-    echo '[WARNING] git config file is not exists.'
+	echo '[WARNING] git config file is not exists.'
 fi
 
 # install k-vim
